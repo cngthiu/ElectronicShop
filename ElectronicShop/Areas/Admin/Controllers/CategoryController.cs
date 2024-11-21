117d81f77bd7aa45ad36707d45085dd189bd369f
Vu Cong Thieu <congthieu.lk02@gmail.com>
2024-11-21 08:47:45 +0700

    Add features Category

---
+++ ElectronicShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,117 @@
+﻿using ElectronicShop.Models;
+using ElectronicShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectronicShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CategoryController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Categories.OrderByDescending(c=>c.Id).ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel category)
+        {
+            //Mếu các dữ liệu đầu vào valid hết thì thêm dữ liệu, không thì in ra lỗi 
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = _dataContext.Categories.FirstOrDefault(p => p.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Category has had in database !");
+                    return View(category);
+                }
+
+                _dataContext.Categories.Add(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Thêm danh mục thành công!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Error!!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryModel category)
+        {
+            //Mếu các dữ liệu đầu vào valid hết thì thêm dữ liệu, không thì in ra lỗi 
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = _dataContext.Categories.FirstOrDefault(p => p.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Category has had in database !");
+                    return View(category);
+                }
+
+                _dataContext.Categories.Update(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Sửa danh mục thành công!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Error!!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+
+            return View(category);
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+            _dataContext.Categories.Remove(category);
+            await _dataContext.SaveChangesAsync();
+            TempData["ErrorMessage"] = "Đã xóadanh mục !!";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ElectronicShop/obj/Debug/net8.0/ElectronicShop.AssemblyInfo.cs
+++ ElectronicShop/obj/Debug/net8.0/ElectronicShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66359c961bf13f2dee6f470da557c5b21ed8fa7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
